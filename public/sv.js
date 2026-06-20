c60d22a913fb4ff09d54fa3f75b7cf97f201500d
Bengisu Üstüntaş <Bengisu@Ekrem-MacBook-Air-2.local>
2026-06-20 14:35:51 +0300

    pwa_kurulumu_tamamlandi

---
+++ public/sv.js
@@ -0,0 +1,11 @@
+self.addEventListener("install", function (e) {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", function (e) {
+  return self.clients.claim();
+});
+
+self.addEventListener("fetch", function (e) {
+  // PWA gereksinimi için boş fetch dinleyicisi
+});
